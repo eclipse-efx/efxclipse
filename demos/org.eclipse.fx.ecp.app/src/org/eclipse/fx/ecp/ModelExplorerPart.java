7577855e82c744700b2e298519e493f06f9d9989
Torsten Sommer <tsommer@tesis.de>
2013-06-03 19:57:11 +0200

    New model element menu item added.

--- demos/org.eclipse.fx.ecp.app/src/org/eclipse/fx/ecp/ModelExplorerPart.java
+++ demos/org.eclipse.fx.ecp.app/src/org/eclipse/fx/ecp/ModelExplorerPart.java
@@ -10,22 +10,39 @@
  *******************************************************************************/
 package org.eclipse.fx.ecp;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Cell;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.inject.Inject;
 
+import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecp.core.ECPProjectManager;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.provider.ItemProvider;
+import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
 import org.eclipse.fx.ecp.dummy.DummyProjectManager;
 import org.eclipse.fx.ecp.dummy.DummyWorkspace;
 import org.eclipse.fx.ecp.provider.ECPItemProviderAdapterFactory;
 import org.eclipse.fx.ecp.ui.ECPModelElementOpener;
+import org.eclipse.fx.emf.edit.ui.AdapterFactoryCellFactory.ICellUpdateListener;
 import org.eclipse.fx.emf.edit.ui.AdapterFactoryTreeCellFactory;
 import org.eclipse.fx.emf.edit.ui.AdapterFactoryTreeItem;
 import org.eclipse.fx.emf.edit.ui.dnd.CellDragAdapter;
@@ -63,15 +80,55 @@ public class ModelExplorerPart {
 
 		cellFactory.addCellCreationListener(new CellDragAdapter());
 
-//		cellFactory.addCellUpdateListener(new ICellUpdateListener() {
-//
-//			@Override
-//			public void updateItem(Cell<?> cell, Object item, boolean empty) {
-//				System.out.println(cell + " updated");
-//
-//				final ContextMenu contextMenu = new ContextMenu();
-//				cell.setContextMenu(contextMenu);
-//				contextMenu.getItems().add(new MenuItem());
+		cellFactory.addCellUpdateListener(new ICellUpdateListener() {
+
+			@Override
+			public void updateItem(Cell<?> cell, Object item, boolean empty) {
+
+				final ContextMenu contextMenu = new ContextMenu();
+				cell.setContextMenu(contextMenu);
+				MenuItem menuItem = new MenuItem("new element");
+				contextMenu.getItems().add(menuItem);
+				
+				menuItem.setOnAction(new EventHandler<ActionEvent>() {
+					
+					@Override
+					public void handle(ActionEvent arg0) {
+						Stage dialog = new Stage();
+						dialog.initModality(Modality.APPLICATION_MODAL);
+//						dialog.initStyle(StageStyle.UTILITY);
+						
+						VBox group = new VBox();
+						
+						Text text = new Text(25, 25, "Hello World!");
+						group.getChildren().add(text);
+						
+						TreeView<Object> treeView2 = new TreeView<>();
+						group.getChildren().add(treeView2);
+						treeView2.setShowRoot(false);
+						
+						ItemProvider rootItem = new ItemProvider();
+						
+						for (String nsURI : Registry.INSTANCE.keySet()) {
+							EPackage ePackage = Registry.INSTANCE.getEPackage(nsURI);
+							rootItem.getChildren().add(ePackage);
+						}
+						
+						ComposedAdapterFactory composedAdapterFactory = new ComposedAdapterFactory();
+						composedAdapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
+						composedAdapterFactory.addAdapterFactory(new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
+						
+						treeView2.setRoot(new AdapterFactoryTreeItem(rootItem, treeView, composedAdapterFactory));
+						AdapterFactoryTreeCellFactory treeCellFactory = new AdapterFactoryTreeCellFactory(composedAdapterFactory);
+						treeView2.setCellFactory(treeCellFactory);
+						
+						Scene scene = new Scene(group);
+						dialog.setScene(scene);
+						dialog.showAndWait();
+					}
+					
+				});
+				
 //				contextMenu.setOnShowing(new EventHandler<WindowEvent>() {
 //
 //					@Override
@@ -81,9 +138,9 @@ public class ModelExplorerPart {
 //					}
 //
 //				});
-//			}
-//
-//		});
+			}
+
+		});
 
 		treeView.setCellFactory(cellFactory);
 
