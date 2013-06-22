f2d4c1a168f4ec4ada048409281e4f7c26618224
Torsten Sommer <tsommer@tesis.de>
2013-06-22 18:52:06 +0200

    Character control added.

---
+++ bundles/runtime/org.eclipse.fx.ecp.ui/src/org/eclipse/fx/ecp/ui/controls/CharacterField.java
@@ -0,0 +1,110 @@
+package org.eclipse.fx.ecp.ui.controls;
+
+import java.util.Objects;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.IndexRange;
+import javafx.scene.control.SkinBase;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecp.edit.ECPControlContext;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.fx.ecp.ui.ECPControl;
+
+public class CharacterField extends ECPControlBase {
+
+	protected TextField textField;
+
+	public CharacterField(IItemPropertyDescriptor propertyDescriptor, ECPControlContext context) {
+		super(propertyDescriptor, context);
+
+		setSkin(new Skin(this));
+
+		textField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldFocused, Boolean newFocused) {
+				if (!newFocused) {
+					Character oldCharacter = (Character) modelElement.eGet(feature);
+					String text = textField.getText();
+					Character newCharacter = text != null && text.length() > 0 ? text.charAt(0) : null;
+					
+					// only commit if the value has changed
+					if (!Objects.equals(oldCharacter, newCharacter)) {
+						Command command = SetCommand.create(editingDomain, modelElement, feature, newCharacter);
+						if (command.canExecute())
+							editingDomain.getCommandStack().execute(command);
+					}
+				}
+			}
+
+		});
+
+		update();
+	}
+
+	@Override
+	protected void update() {
+		Object value = modelElement.eGet(feature);
+		textField.setText(value.toString());
+	}
+
+	protected class Skin extends SkinBase<CharacterField> {
+
+		protected Skin(CharacterField control) {
+			super(control);
+
+			HBox hBox = new HBox();
+			getChildren().add(hBox);
+
+			textField = new TextField() {
+
+				@Override
+				public void replaceText(int start, int end, String text) {
+					String currentText = getText();
+					StringBuffer sb = new StringBuffer(currentText);
+					sb.replace(start, end, text);
+					String tmp = sb.toString();
+
+					if (validate(tmp))
+						super.replaceText(start, end, text);
+				}
+
+				@Override
+				public void replaceSelection(String text) {
+					IndexRange selection = getSelection();
+					int start = selection.getStart();
+					int end = selection.getEnd();
+					StringBuffer sb = new StringBuffer(getText());
+					sb.replace(start, end, text);
+					String tmp = sb.toString();
+
+					if (validate(tmp))
+						super.replaceSelection(text);
+				}
+
+				private boolean validate(String tmp) {
+					return tmp.length() <= 1;
+				}
+
+			};
+			hBox.getChildren().add(textField);
+			textField.setPrefColumnCount(1);
+		}
+
+	}
+
+	public static class Factory implements ECPControl.Factory {
+
+		@Override
+		public ECPControlBase createControl(IItemPropertyDescriptor itemPropertyDescriptor, ECPControlContext context) {
+			return new CharacterField(itemPropertyDescriptor, context);
+		}
+
+	}
+
+}
--- bundles/runtime/org.eclipse.fx.ecp.ui/src/org/eclipse/fx/ecp/ui/controls/TextFieldControl.java
+++ bundles/runtime/org.eclipse.fx.ecp.ui/src/org/eclipse/fx/ecp/ui/controls/TextFieldControl.java
@@ -4,16 +4,11 @@ import java.util.Objects;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.scene.control.SkinBase;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 
 import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.common.util.Diagnostic;
-import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.util.Diagnostician;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecp.edit.ECPControlContext;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -22,51 +17,25 @@ import org.eclipse.fx.ecp.ui.ECPControl;
 public class TextFieldControl extends ECPControlBase {
 
 	protected TextField textField;
-	protected EDataTypeValueHandler valueHandler;
 
 	public TextFieldControl(IItemPropertyDescriptor propertyDescriptor, ECPControlContext context) {
 		super(propertyDescriptor, context);
-		
+
 		setSkin(new Skin(this));
-		
-		valueHandler = new EDataTypeValueHandler((EDataType) feature.getEType());
-
-		textField.textProperty().addListener(new ChangeListener<String>() {
-
-			@Override
-			public void changed(ObservableValue<? extends String> observableValue, String oldText, String newText) {
-				final String message = valueHandler.isValid(newText);
-				ObservableList<String> styleClass = textField.getStyleClass();
-				if (message == null) {
-					styleClass.remove("invalid");
-				} else {
-					if (!styleClass.contains("invalid"))
-						styleClass.add("invalid");
-				}
-			}
-
-		});
 
 		textField.focusedProperty().addListener(new ChangeListener<Boolean>() {
 
 			@Override
 			public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldFocused, Boolean newFocused) {
 				if (!newFocused) {
-					Object oldValue = modelElement.eGet(feature);
-					String text = textField.getText();
-					String message = valueHandler.isValid(text);
+					Object oldText = modelElement.eGet(feature);
+					String newText = textField.getText();
 
-					if (message == null) {
-						Object newValue = valueHandler.toValue(text);
-
-						// only commit if the value has changed
-						if (!Objects.equals(oldValue, newValue)) {
-							Command command = SetCommand.create(editingDomain, modelElement, feature, newValue);
-							if (command.canExecute())
-								editingDomain.getCommandStack().execute(command);
-						}
-					} else {
-						System.err.println(message);
+					// only commit if the value has changed
+					if (!Objects.equals(oldText, newText)) {
+						Command command = SetCommand.create(editingDomain, modelElement, feature, newText);
+						if (command.canExecute())
+							editingDomain.getCommandStack().execute(command);
 					}
 				}
 			}
@@ -78,22 +47,22 @@ public class TextFieldControl extends ECPControlBase {
 
 	@Override
 	protected void update() {
-		Object value = modelElement.eGet(feature);
-		textField.setText(valueHandler.toString(value));
+		String value = (String) modelElement.eGet(feature);
+		textField.setText(value);
 	}
 
-	class Skin extends SkinBase<TextFieldControl> {
-		
-		Skin(TextFieldControl control) {
+	protected class Skin extends SkinBase<TextFieldControl> {
+
+		protected Skin(TextFieldControl control) {
 			super(control);
-			
-			VBox vBox = new VBox();
-			getChildren().add(vBox);
-			
+
+			HBox hBox = new HBox();
+			getChildren().add(hBox);
+
 			textField = new TextField();
-			vBox.getChildren().add(textField);
+			hBox.getChildren().add(textField);
 		}
-		
+
 	}
 
 	public static class Factory implements ECPControl.Factory {
@@ -101,54 +70,6 @@ public class TextFieldControl extends ECPControlBase {
 		@Override
 		public ECPControlBase createControl(IItemPropertyDescriptor itemPropertyDescriptor, ECPControlContext context) {
 			return new TextFieldControl(itemPropertyDescriptor, context);
-		}
-
-	}
-
-	/**
-	 * A delegate for handling validation and conversion for data type values.
-	 */
-	protected static class EDataTypeValueHandler {
-		protected EDataType eDataType;
-
-		public EDataTypeValueHandler(EDataType eDataType) {
-			this.eDataType = eDataType;
-		}
-
-		public String isValid(Object object) {
-			Object value;
-			try {
-				value = eDataType.getEPackage().getEFactoryInstance().createFromString(eDataType, (String) object);
-			} catch (Exception exception) {
-				String message = exception.getClass().getName();
-				int index = message.lastIndexOf('.');
-				if (index >= 0) {
-					message = message.substring(index + 1);
-				}
-				if (exception.getLocalizedMessage() != null) {
-					message = message + ": " + exception.getLocalizedMessage();
-				}
-				return message;
-			}
-			Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eDataType, value);
-			if (diagnostic.getSeverity() == Diagnostic.OK) {
-				return null;
-			} else {
-				return (diagnostic.getChildren().get(0)).getMessage().replaceAll("'", "''").replaceAll("\\{", "'{'"); // }}
-			}
-		}
-
-		public String isValid(String text) {
-			return isValid((Object) text);
-		}
-
-		public Object toValue(String string) {
-			return EcoreUtil.createFromString(eDataType, string);
-		}
-
-		public String toString(Object value) {
-			String result = EcoreUtil.convertToString(eDataType, value);
-			return result == null ? "" : result;
 		}
 
 	}
