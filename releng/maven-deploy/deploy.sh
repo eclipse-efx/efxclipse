cb2c721dbe45298076a76067769aad433c670206
Tom Schindl <tom.schindl@bestsolution.at>
2014-08-12 00:44:15 +0200

    added deploy settings

--- releng/maven-deploy/deploy.sh
+++ releng/maven-deploy/deploy.sh
@@ -1,4 +1,4 @@
-/shared/common/apache-maven-3.0.5/bin/mvn deploy:deploy-file -DgroupId=org.eclipse.fx \
+/shared/common/apache-maven-3.0.5/bin/mvn deploy:deploy-file -s /shared/common/apache-maven-latest/conf/settings.xml -DgroupId=org.eclipse.fx \
   -DartifactId=org.eclipse.fx.ui.panes \
   -Dversion=1.0.0-SNAPSHOT \
   -Dpackaging=jar \
