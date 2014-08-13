7abccc5759e5e959724fc737452e5c7e0dca00f3
Tom Schindl <tom.schindl@bestsolution.at>
2014-08-13 09:23:21 +0200

    added dummy

--- releng/maven-deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-/shared/common/apache-maven-3.0.5/bin/mvn deploy:deploy-file -s /shared/common/apache-maven-latest/conf/settings.xml -DgroupId=org.eclipse.fx \
-  -DartifactId=org.eclipse.fx.ui.panes \
-  -Dversion=1.0.0-SNAPSHOT \
-  -Dpackaging=jar \
-  -Dfile=bundles/runtime/org.eclipse.fx.ui.panes/target/org.eclipse.fx.ui.panes-1.0.0-SNAPSHOT.jar \
-  -DrepositoryId=repo.eclipse.org \
-  -Durl=https://repo.eclipse.org/content/repositories/efxclipse-snapshots/
